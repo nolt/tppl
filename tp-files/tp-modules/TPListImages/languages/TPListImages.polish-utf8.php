988883426150e774d7d5bbe030b4bc1ef0e1c987
Nolt <nolt@wizzi.pl>
2018-12-02 22:58:57 +0100

    brakujące tłuamczenie nowego pliku

---
+++ tp-files/tp-modules/TPListImages/languages/TPListImages.polish-utf8.php
@@ -0,0 +1,11 @@
+<?php
+
+$txt['tp-listimage-settings'] 	= 'Lista wysłanych obrazków w TinyPortal';
+$txt['tp-listimage-list'] 		= 'Lista obrazków';
+$txt['tp-listimage-remove'] 	= 'Usuń obrazek';
+$txt['tp-listimage-username']   = 'Użytkownik';
+$txt['tp-listimage-intro']   	= 'Ten ekran pozwala na wylistowanie oraz skasowanie obrazków, wysłanych przez użytkowników.';
+
+$txt['permissionname_tp_can_list_images'] = 'Zarządzaj ilustracjami TP';
+$txt['permissionhelp_tp_can_list_images'] = 'Pozwól użytkownikom usuwać obrazki z katalogu obrazów TinyPortal.';
+$txt['cannot_tp_can_list_images'] = 'Przepraszamy, nie masz uprawnień do zarządzania obrazkami w artykule.';
